bd84668f17f9b59a9f7c7156dc96b273d9b8a9f2
Karen V <karenvo@usc.edu>
2019-03-08 21:13:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,48 +1,5 @@
 # Make your shoe class here!
 class Shoes
-
-  def initialize(brand)
-    @brand=brand
-  end
-
-  def brand=(brand)
-    @brand=brand
-  end
-
-  def brand
-    @brand
-  end
-
-  def color=(color)
-    @color=color
-  end
-
-  def color
-    @color
-  end
-
-  def size=(size)
-    @size=size
-  end
-
-  def size
-    @size
-  end
-
-  def material=(material)
-    @material=material
-  end
-
-  def material
-    @material
-  end
-
-  def condition=(condition)
-    @condition=condition
-  end
-
-  def condition
-    @condition
-  end
-
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 end
